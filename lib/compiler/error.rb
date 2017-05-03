d9ad728b7bec5a2bcd354d93e9bd469ebfee7b24
Minqi Pan <pmq2001@gmail.com>
2017-05-03 11:45:35 +0800

    .

--- lib/compiler/error.rb
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2017 Minqi Pan <pmq2001@gmail.com>
-# 
-# This file is part of Node.js Compiler, distributed under the MIT License
-# For full terms see the included LICENSE file
-
-class Compiler
-  class Error < ::RuntimeError; end
-end
